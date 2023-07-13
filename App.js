86aaa3c0b9c21da9471b722464e053be6bde7d0b
Your Name <you@example.com>
2023-07-14 02:52:30 +0530

    Drawer Navigation Aded

--- App.js
+++ App.js
@@ -1,37 +1,40 @@
 import { View, Text, Button } from 'react-native'
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
-const HomeScreen = ({ navigation }) => {
+const Drawer = createDrawerNavigator();
+
+function HomeScreen({ navigation }) {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Button
+          onPress={() => navigation.navigate('Notifications')}
+          title="Go to notifications"
+        />
+      </View>
+    );
+  }
+
+function NotificationsScreen({ navigation }) {
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Home Screen</Text>
-            <Button
-                title='Go TO Details Screen'
-                onPress={() => navigation.navigate("Details")}
-            />
+            <Button onPress={() => navigation.goBack()} title="Go back home" />
         </View>
     );
 }
 
-const DetailsScreen = () => {
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Details Screennnn</Text>
-        </View>
-    );
-}
 
-const Stack = createNativeStackNavigator();
 
 export default function App() {
     return (
+
         <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name="Home" component={HomeScreen} />
-                <Stack.Screen name="Details" component={DetailsScreen} />
-            </Stack.Navigator>
+            <Drawer.Navigator initialRouteName="Home">
+                <Drawer.Screen name="Home" component={HomeScreen} />
+                <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+            </Drawer.Navigator>
         </NavigationContainer>
+
     )
 }
